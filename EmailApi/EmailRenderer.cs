099b781b1796bae5b927cb68418845c9a85c2929
David <dave.eidson@gmail.com>
2018-08-04 00:05:50 -0500

    Created a email renderer

---
+++ EmailApi/EmailRenderer.cs
@@ -0,0 +1,21 @@
+﻿using SharpRazor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmailApi
+{
+    public class EmailRenderer
+    {
+        private Razorizer renderer = new Razorizer();
+        private const string Template = "<html><head><title>@Model.From</title></head><body><p>@Model.Body</p></body></html>";
+
+        //public string Render(EmailMessage message) => renderer.Parse(Template, message);
+
+        public string Render(EmailMessage message)
+        {
+            return renderer.Parse(Template, message);
+        }
+
+    }
+}
---
+++ EmailTests/EmailRendererFixture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using EmailApi;
+
+namespace EmailTests
+{
+    public class EmailRendererFixture
+    {
+        [Fact]
+        public void CanRender()
+        {
+            EmailRenderer renderer = new EmailRenderer();
+            EmailMessage message = new EmailMessage();
+
+            message.From = "The beginning of the message";
+            message.Header = "the header";
+            message.Body = "the body";
+
+            var result = renderer.Render(message);
+        }
+    }
+}
